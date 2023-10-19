f935161fa4274dd36e18a4c0eeb51f4d81e8bbef
lconcann <lconcann@stevens.edu>
2023-10-19 12:30:51 -0400

    Created react app with Brandon's html/css

---
+++ agilemusic/src/app/page.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    
+    <main>
+      <head>
+        <title>Web Music Player</title>
+      </head>
+    <body>
+        <Link href="/login" key="login">
+          <button className="w-40 h-10 text-center border-r border-black hover:bg-blue-100">Login</button>
+        </Link>
+        <h1>Web Music Player</h1>
+        <audio id="audioPlayer" controls>
+            <source src="your-audio-file.mp3" type="audio/mpeg"></source>
+            Your browser does not support the audio element.
+        </audio>
+        <button id="playButton">Play</button>
+        <button id="pauseButton">Pause</button>
+        <button id="skipButton">Skip</button>
+        <button id="goBackButton">Go Back</button>
+        <button id="playlistButton">Change Playlist</button>
+     </body>     
+    </main>
+  )
+}
