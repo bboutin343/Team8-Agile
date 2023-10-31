bda304745efdfca5330aff362dcfdfd1aed0135a
bboutin343 <bboutin@stevens.edu>
2023-10-31 14:58:00 -0400

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -1,4 +1,5 @@
 import statistics
+import json
 from collections import Counter
 
 def likedsongs():
@@ -7,6 +8,9 @@ def likedsongs():
 
 def extractinfo(songs):
     #extracts the songs and metadata
+    data = []
+    for i in songs:
+        data.append([json.loads(i)['name']],[json.loads(i)['album']['release_date']],[json.loads(i)['artists']['genres']])
     return data
 
 def dates(releasedates):
@@ -26,8 +30,19 @@ def genres(genrelist):
     merged_list = tuple(zip(uniques, percents))
     return merged_list
 
-def createplaylist(daterange, genres, size):
+def createplaylist(daterange, genres, size=20):
     #find songs from the spotify database that have a releasedate in the
     #given range and matching genre, then create a playlist of a similar
     #composition of genre to the user's liked songs and indicated size.
     return playlist
+
+liked = likedsongs()
+likedinfo = extractinfo(liked)
+date = []
+genre = []
+for i in likedinfo:
+    date.append(i[1])
+    genre.append(i[2])
+daterange = dates(date)
+genrecomp = genres(genre)
+playlist = createplaylist(daterange, genrecomp, 20)
