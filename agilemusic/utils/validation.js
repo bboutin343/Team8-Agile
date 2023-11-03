89da94852fb0aac2d9c5ccd25f0e6ad747438486
therman77 <127142043+therman77@users.noreply.github.com>
2023-11-02 23:32:41 -0400

    Added validation

---
+++ agilemusic/utils/validation.js
@@ -0,0 +1,11 @@
+export function validateStringInput(input, inputName) {
+    if (!input || typeof input !== 'string') {
+        throw `Error: ${inputName} must be a valid string`;
+    }
+}
+
+export function validateDateInput(dateString, inputName) {
+    if (!dateString || Object.prototype.toString.call(new Date(dateString)) !== '[object Date]' || isNaN(new Date(dateString).getTime())) {
+        throw `Error: ${inputName} must be a valid date ("YYYY-MM-DD")`;
+    }
+}
