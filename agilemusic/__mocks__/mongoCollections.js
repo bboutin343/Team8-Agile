a67fdbd7e7e6a449bceaa476c56367379e65448e
therman77 <127142043+therman77@users.noreply.github.com>
2023-12-06 13:41:01 -0500

    Added positive and negative users.js tests and corresponding mocks

--- agilemusic/__mocks__/mongoCollections.js
+++ agilemusic/__mocks__/mongoCollections.js
@@ -20,9 +20,53 @@ const songsCollectionMock = {
 };
 
 export const songs = jest.fn().mockImplementation(() => songsCollectionMock);
-
 export const setMockInsertOneBehavior = behavior => {
   mockInsertOneBehavior = behavior;
-}
+};
+
+let userData = {
+    _id: new ObjectId('507f191e810c19729de860ea'),
+    username: 'TestUser',
+    likedSongs: [],
+    playlists: [{ playlistId: new ObjectId('507f191e810c19729de860eb'), songs: [] }]
+};
+
+let mockUserInsertOneBehavior = user => Promise.resolve({ insertedCount: 1, insertedId: new ObjectId() });
+let mockUserUpdateOneBehavior = (query, update) => {
+    if (query._id.toString() === '507f191e810c19729de860ea') {
+        if (update.$push && update.$push.playlists) {
+            userData.playlists.push(update.$push.playlists);
+            return Promise.resolve({ matchedCount: 1, modifiedCount: 1 });
+        }
+    }
+    return Promise.resolve({ matchedCount: 0, modifiedCount: 0 });
+};
+
+let mockUserFindOneBehavior = query => {
+    if (query._id && query._id.toString() === '507f191e810c19729de860ea') {
+        return Promise.resolve(userData);
+    }
+    if (query._id && query["playlists.playlistId"]) {
+        return Promise.resolve(userData);
+    }
+    return Promise.resolve(null);
+};
+
+const usersCollectionMock = {
+    insertOne: jest.fn(user => mockUserInsertOneBehavior(user)),
+    updateOne: jest.fn((query, update) => mockUserUpdateOneBehavior(query, update)),
+    findOne: jest.fn(query => mockUserFindOneBehavior(query)),
+};
+
+export const users = jest.fn().mockImplementation(() => usersCollectionMock);
+export const setMockUserInsertOneBehavior = behavior => {
+    mockUserInsertOneBehavior = behavior;
+};
+export const setMockUserUpdateOneBehavior = behavior => {
+    mockUserUpdateOneBehavior = behavior;
+};
+export const setMockUserFindOneBehavior = behavior => {
+    mockUserFindOneBehavior = behavior;
+};
 
 export { ObjectId };
